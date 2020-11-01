f61fdf50c63e5b95e547d3824ab16acac30de879
lbmann <l.bmann@yahoo.com>
2020-10-31 20:09:16 -0400

    updating script to pull unnecessary console logs used to debug

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 var passLength = document.getElementById('passLength').value;
 
-
-
 var alphaLowerValues = "abcdefghijklmnopqrstuvwxyz";
 var alphaUpperValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericValues = "1234567890";
@@ -11,21 +9,15 @@ var specialValues = "!#$%&()/?@{}~";
 
 
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+defaultPassLength();
 
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", launchForm);
 
-// window.onload = updateLengthOutput(passLength);
+document.getElementById("submitInput").addEventListener("click", formValidation);
 
-window.onload = defaultPassLength();
+document.getElementById("password").addEventListener("click", copyPassword);
+
+
 
 function launchForm() {
   document.getElementById("inputForm").style.visibility = "visible";
@@ -39,20 +31,12 @@ function defaultPassLength() {
   document.getElementById("passLength").defaultValue = "8";
 }
 
-// function updateLengthOutput(passLength) {
-//   document.getElementById('passLengthOutput').innerHTML = passLength;
-//   return passLength;
-// }
-
-document.getElementById("submitInput").addEventListener("click", formValidation);
 
 function formValidation() {
 
   event.preventDefault();
 
   passLength = document.getElementById('passLength').value;
-
-  console.log(passLength);
 
   if (document.querySelectorAll('input[type="checkbox"]:checked').length > 0 && passLength >= 8 && passLength <= 128) {
     generatePassword();
@@ -64,6 +48,7 @@ function formValidation() {
   else {
     alert("You must select at least one checkbox to continue!")
   }
+
 }
 
 function generatePassword() {
@@ -71,48 +56,36 @@ function generatePassword() {
     var passwordAddition = "";
     var password = "";
 
-    if (document.getElementById("uppercase").checked==true) {
-        passwordChar += alphaUpperValues;
-    }
+      if (document.getElementById("uppercase").checked==true) {
+          passwordChar += alphaUpperValues;
+      }
 
-    if (document.getElementById("lowercase").checked==true) {
-        passwordChar += alphaLowerValues;
-    }
+      if (document.getElementById("lowercase").checked==true) {
+          passwordChar += alphaLowerValues;
+      }
 
-    if (document.getElementById("numeric").checked==true) {
-        passwordChar += numericValues;
-    }
+      if (document.getElementById("numeric").checked==true) {
+          passwordChar += numericValues;
+      }
 
-    if (document.getElementById("specialChar").checked==true) {
-        passwordChar += specialValues;
-    }
+      if (document.getElementById("specialChar").checked==true) {
+          passwordChar += specialValues;
+      }
 
     passwordChar = passwordChar.split("");
 
-    console.log(passwordChar);
-
     var passwordCharLength = passwordChar.length;
 
-    console.log(passwordCharLength);
 
-    console.log(passLength);
+      for (i = 0; i < passLength; i++) {
+          randomNumber(0, (passwordCharLength-1));
+          password += passwordAddition;
+      }
 
-    for (i = 0; i < passLength; i++) {
-
-        randomNumber(0, (passwordCharLength-1));
-        password += passwordAddition;
-        console.log(password);
-
-    }
-
-    function randomNumber(min, max) {
-      charChoice = Math.floor(Math.random() * (max - min + 1) + min);
-      console.log(charChoice);
-      passwordAddition = passwordChar[charChoice];
-      console.log(passwordAddition);
-    }
-
-    console.log(password);
+      function randomNumber(min, max) {
+        charChoice = Math.floor(Math.random() * (max - min + 1) + min);
+        passwordAddition = passwordChar[charChoice];
+      }
 
     document.getElementById("password").innerHTML = password;
 
@@ -122,7 +95,6 @@ function generatePassword() {
 }
 
 
-document.getElementById("password").addEventListener("click", copyPassword);
 
 function copyPassword() {
   var passwordCopy = document.getElementById("password");
